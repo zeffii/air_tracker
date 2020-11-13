7639526c2e54b4e7e8357c9c02564d800a91f6f9
zeffii <tetha.z@gmail.com>
2020-11-13 19:50:53 +0100

    rearrange comments

--- src/pattern.cpp
+++ src/pattern.cpp
@@ -30,17 +30,6 @@ Pattern::Pattern(SDL_Renderer *renderer, const char* pattern_path)
     std::vector<string> pattern_data;
     readPattern(pattern_path, pattern_data);
 
-    // 001 C-5 83 D#5 80 G-5 80 ... ..  20 50 40 4E  20 EE 30
-    // TTT = tick
-    // S* = space
-    // H* = hex
-    /* 
-       TTTSNNNSHHSNNNSHHSNNNSHHSNNNSHHSSHHSHHSHHSHHSSHHSHHSHH
-           0   1  2   3  4   5  6   7   8  9  10 11  12 13 14  concept index
-           3   2  3   2  3   2  3   2   2  2  2  2   2  2  2   length
-           0   4  7   11 14  18 21  24  28 31 34 37  41 44 47  index start
-    */
-
     TTF_Font *font = TTF_OpenFont("res/consola.ttf", 11);
     if (!font) { cerr << "failed to load font\n"; }
 
--- src/rect.cpp
+++ src/rect.cpp
@@ -23,30 +23,50 @@ void Rect::draw() const {
 
 void Rect::pollEvents(SDL_Event &event){
 
-    //int x_offset = 20;
+    int x_offset = 20;
     int y_offset = 20;
     int charwidth = 6;
+    int nchars_inrow = 54;  // includes tick alignment
     int nrows = 16;
     int line_height = 13;
+
+    /*
+
+        this will be automated!!
+
+        001 C-5 83 D#5 80 G-5 80 ... ..  20 50 40 4E  20 EE 30
+        TTT = tick
+        S* = space
+        H* = hex
+
+                                        28           41
+               3  6   10 13  17 20  24 27  31 34 37 40  44 47
+        TTTSNNNSHHSNNNSHHSNNNSHHSNNNSHHSSHHSHHSHHSHHSSHHSHHSHH
+            0   1  2   3  4   5  6   7   8  9  10 11  12 13 14  concept index
+            3   2  3   2  3   2  3   2   2  2  2  2   2  2  2   length
+            0   4  7   11 14  18 21  24  29 32 35 38  42 45 48  index start
+    */    
 
     if (event.type == SDL_KEYDOWN){
         switch (event.key.keysym.sym) {
             case SDLK_LEFT:
                 _x -= charwidth;
+                if (_x <= (x_offset + 3*6)) { _x = 20 + (nchars_inrow-1) * charwidth; }
                 column_index -= 1;
                 break;
             case SDLK_RIGHT:
                 _x += charwidth;
+                if (_x >= (x_offset + nchars_inrow * charwidth)) { _x = x_offset + 4*6; }
                 column_index += 1;
                 break;
             case SDLK_UP:
                 _y -= line_height;
-                if (_y <= y_offset){ _y = y_offset + (nrows-1)*line_height; } 
+                if (_y < y_offset){ _y = y_offset + (nrows-1) * line_height; } 
                 row_index -= 1;
                 break;
             case SDLK_DOWN:
                 _y += line_height;
-                if (_y >=(y_offset + nrows*line_height)){ _y = y_offset;}
+                if (_y >= (y_offset + nrows * line_height)){ _y = y_offset;}
                 row_index += 1;
                 break;
         }
