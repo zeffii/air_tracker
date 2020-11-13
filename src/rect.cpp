b4c13a4d5f65e9f957602a3cfc8a9be19ffdb234
zeffii <tetha.z@gmail.com>
2020-11-14 00:10:53 +0100

    implement cursor hopping

--- src/rect.cpp
+++ src/rect.cpp
@@ -20,48 +20,31 @@ void Rect::draw() const {
     SDL_SetRenderDrawColor(Window::renderer, 0xE9, 0x3E, 0x4D, 0xFF );        
     SDL_RenderDrawRect(Window::renderer, &outlineRect );    
 }
-/*
-void Rect::pollEvents(SDL_Event &event){
 
-    int x_offset = 20;
-    int y_offset = 20;
-
-    //
-    //                                  28           41
-    //         3  6   10 13  17 20  24 27  31 34 37 40  44 47
-    //  TTTSNNNSHHSNNNSHHSNNNSHHSNNNSHHSSHHSHHSHHSHHSSHHSHHSHH
-    //      0   1  2   3  4   5  6   7   8  9  10 11  12 13 14  concept index
-    //      3   2  3   2  3   2  3   2   2  2  2  2   2  2  2   length
-    //      0   4  7   11 14  18 21  24  29 32 35 38  42 45 48  index start
-    //    
-
-    if (event.type == SDL_KEYDOWN){
-        switch (event.key.keysym.sym) {
-            case SDLK_LEFT:
-                column_index -= 1;
-                _x -= charwidth;
-                if (_x <= (x_offset + 3*6)) { _x = 20 + (nchars_inrow-1) * charwidth; }
-                break;
-            case SDLK_RIGHT:
-                column_index += 1;
-                _x += charwidth;
-                if (_x >= (x_offset + nchars_inrow * charwidth)) { _x = x_offset + 4*6; }
-                break;
-            case SDLK_UP:
-                row_index -= 1;
-                _y -= line_height;
-                if (_y < y_offset){ _y = y_offset + (nrows-1) * line_height; } 
-                break;
-            case SDLK_DOWN:
-                row_index += 1;
-                _y += line_height;
-                if (_y >= (y_offset + nrows * line_height)){ _y = y_offset;}
-                break;
-        }
+int find_int_in_array(int num_to_find, int numbers[], int arraysize){
+    for(int i = 0; i < arraysize; i++){
+         if(numbers[i] == num_to_find){ return 1; }
     }
-    cout << column_index << ", " << row_index << endl;
+    return 0;
 }
-*/
+
+
+void carrot_hop_forward(int &c_index){
+    int numbers[12] = {3, 6, 10, 13, 17, 20, 24, 31, 34, 37, 44, 47};
+    int spacer_numbers[2] = {27, 40};
+
+    if (find_int_in_array(c_index, numbers, 12)) { c_index += 1; }
+    else if (find_int_in_array(c_index, spacer_numbers, 2)) { c_index += 2; }
+}
+
+void carrot_hop_backward(int &c_index){
+    int numbers[12] = {3, 6, 10, 13, 17, 20, 24, 31, 34, 37, 44, 47};
+    int spacer_numbers[2] = {28, 41};
+
+    if (find_int_in_array(c_index, numbers, 12)) { c_index -= 1; }
+    else if (find_int_in_array(c_index, spacer_numbers, 2)) { c_index -= 2; }
+}
+
 
 void Rect::pollEvents(SDL_Event &event){
 
@@ -81,12 +64,15 @@ void Rect::pollEvents(SDL_Event &event){
         switch (event.key.keysym.sym) {
             case SDLK_LEFT:
                 column_index -= 1;
+                carrot_hop_backward(column_index);
                 if (column_index < 0) { column_index = nchars_inrow-5; }
                 _x = (x_offset + 4 * charwidth) + (column_index * charwidth);
                 
                 break;
             case SDLK_RIGHT:
                 column_index += 1;
+                //column_index += carrot_hop_forward(column_index);
+                carrot_hop_forward(column_index);
                 column_index %= nchars_inrow-4;
                 _x = (x_offset + 4 * charwidth) + (column_index * charwidth);
                 
