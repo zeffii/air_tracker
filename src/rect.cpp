5e061824da3af80cab9c682aa82ebb7c7e0eb0e6
zeffii <tetha.z@gmail.com>
2020-11-13 16:12:20 +0100

    revamp cursor visual

--- src/rect.cpp
+++ src/rect.cpp
@@ -16,29 +16,37 @@ void Rect::draw() const {
     SDL_SetRenderDrawColor(Window::renderer, _r, _g, _b, _a);
     SDL_RenderFillRect(Window::renderer, &rect);
 
-    SDL_Rect outlineRect = {_x, _y, _w, _h};
-    SDL_SetRenderDrawColor(Window::renderer, 0xA0, 0x00, 0x90, 0xFF );        
+    SDL_Rect outlineRect = {_x-1, _y-1, _w+2, _h};
+    SDL_SetRenderDrawColor(Window::renderer, 0xE9, 0x3E, 0x4D, 0xFF );        
     SDL_RenderDrawRect(Window::renderer, &outlineRect );    
 }
 
 void Rect::pollEvents(SDL_Event &event){
 
+    //int x_offset = 20;
+    int y_offset = 20;
+    int charwidth = 6;
+    int nrows = 16;
+    int line_height = 13;
+
     if (event.type == SDL_KEYDOWN){
         switch (event.key.keysym.sym) {
             case SDLK_LEFT:
-                _x -= 6;
+                _x -= charwidth;
                 column_index -= 1;
                 break;
             case SDLK_RIGHT:
-                _x += 6;
+                _x += charwidth;
                 column_index += 1;
                 break;
             case SDLK_UP:
-                _y -= 13;
+                _y -= line_height;
+                if (_y <= y_offset){ _y = y_offset + (nrows-1)*line_height; } 
                 row_index -= 1;
                 break;
             case SDLK_DOWN:
-                _y += 13;
+                _y += line_height;
+                if (_y >=(y_offset + nrows*line_height)){ _y = y_offset;}
                 row_index += 1;
                 break;
         }
