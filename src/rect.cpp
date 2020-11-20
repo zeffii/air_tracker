c3374213a063413579d6a2eb15b20bef4541c3b1
zeffii <tetha.z@gmail.com>
2020-11-20 17:35:28 +0100

    drops comment

--- src/rect.cpp
+++ src/rect.cpp
@@ -32,15 +32,6 @@ void Rect::pollEvents(SDL_Event &event, Pattern &mypat, Window &window, Selector
     int nchars_inrow = mypat.get_nchars_in_row();
     int nrows = mypat.get_nrows_in_column();
 
-    /*
-    if SHIFT keydown 
-        => record column_index_start, row_index_start
-    while shift pressed 
-        => record column_index and row_index as column_index_start and row_index_end
-    
-    // SDL_PRESSED or SDL_RELEASED
-
-    */
     if (event.type == SDL_KEYDOWN){
         switch (event.key.keysym.sym) {
 
