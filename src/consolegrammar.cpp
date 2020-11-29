2891e47de4967cf63f34f4c393d20117735fd58f
zeffii <tetha.z@gmail.com>
2020-11-29 19:33:26 +0100

    adds tentative try catch for console input

--- src/consolegrammar.cpp
+++ src/consolegrammar.cpp
@@ -12,16 +12,33 @@ ConsoleGrammar::ConsoleGrammar(Selector &selection, Pattern &mypat, string comma
     cout << ">" << commands << endl;
     cout << "current selection state: " << selection.get_selector_state() << endl;
 
-    if (selection.get_selector_state()){
-
-        Selection_Range sr = {};
-        mypat.get_corrected_selection_range(selection, sr);
-
-        int char_offset = 4;
-        int selection_length = (sr.last_col_idx - sr.first_col_idx) + 1;
-        int selection_start = sr.first_col_idx + char_offset;
-        cout << selection_start << ", " << selection_length << endl;
+    if (!(selection.get_selector_state())){
+        // do something else.
+        return;
     }
+
+    string operand = "";
+    try {
+        operand = commands.substr(0, 3);
+        if (operand == "amp") {
+            cout << "Attempting to amplify.";
+        } else {
+            throw 99;
+        }
+    }
+    catch (...) {
+        cout << "Please read the console manual.\n";
+        return;
+    }
+
+    Selection_Range sr = {};
+    mypat.get_corrected_selection_range(selection, sr);
+
+    int char_offset = 4;
+    int selection_length = (sr.last_col_idx - sr.first_col_idx) + 1;
+    int selection_start = sr.first_col_idx + char_offset;
+    cout << selection_start << ", " << selection_length << endl;
+    
 };
 
 
