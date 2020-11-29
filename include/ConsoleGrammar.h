81d211eca6cfaffa13e62357765a3818cc13af63
zeffii <tetha.z@gmail.com>
2020-11-29 14:11:14 +0100

    add imports

---
+++ include/ConsoleGrammar.h
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include <iostream>
+// #include "SelectRange.h"
+#include "Pattern.h"
+#include "Hex_functions.h"
+
+
+using namespace std;
+
+class ConsoleGrammar {
+
+    public:
+        ConsoleGrammar(int w);
+
+    private:
+        vector<string> replacement_data;
+};
---
+++ src/consolegrammar.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+#include <iostream>
+// #include "SelectRange.h"
+#include "Pattern.h"
+#include "Hex_functions.h"
+#include "ConsoleGrammar.h"
+
+using namespace std;
+
+ConsoleGrammar::ConsoleGrammar(int w){};
+
+
