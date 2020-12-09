550bea39382887670b84b8e73e9d68cf89577bbd
zeffii <tetha.z@gmail.com>
2020-12-09 23:11:31 +0100

    adds osc drawing

--- include/Oscilloscope.h
+++ include/Oscilloscope.h
@@ -3,5 +3,6 @@
 #include <math.h> 
 #include "Abstractions.h"
 
+void DrawDottedLine(SDL_Renderer *renderer, Line line, SDL_Color color);
 void SDLX_draw_dotted_line(SDL_Renderer *renderer, Line line, SDL_Color color);
 void draw_oscilloscope(SDL_Renderer *renderer, SDL_Rect osc_rect);
