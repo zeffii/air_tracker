ea0d00853cb471ee7225fe3bb0dc4e1db71c7e45
zeffii <tetha.z@gmail.com>
2020-12-16 18:59:13 +0100

    add map func

--- src/functions.cpp
+++ src/functions.cpp
@@ -53,6 +53,10 @@ vector<float> quadratic_interval(float start, float end, float thru){
 };
 */
 
+float map(float x, float in_min, float in_max, float out_min, float out_max){
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+};
+
 bool is_string_numeric(string str){
     /*
     numeric strings may contain numbers, and at most one decimal point. 
